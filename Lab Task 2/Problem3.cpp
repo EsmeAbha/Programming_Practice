0b67e9ad9bbe14d657143969e808d4f66d29f1eb
Esm-e Moula Chowdhury Abha <100505484+EsmeAbha@users.noreply.github.com>
2022-09-27 22:55:10 +0600

    Update Problem3.cpp

--- Lab Task 2/Problem3.cpp
+++ Lab Task 2/Problem3.cpp
@@ -3,17 +3,23 @@ using namespace std;
 int main()
 {
     int count=0;
-    int arr[]={112,223,21,4,343,5,343,21,112,89};
+    int count1=0;
+    int arr[]={112,223,21,4,343,5,33,211,112,89};
     int arr2[10];
     for(int i=0;i<10;i++){
         int j;
         for(j=0;j<i;j++)
-            if(arr[i]==arr[j])
+            if(arr[i]==arr[j]){
+              count1++;
                break;
+            }
             if(i==j){
-              arr2[i]=arr[i];
+              arr2[count]=arr[i];
               count++;
             }
+    }
+    if(count1==0){
+      cout<<"\nThe elements are already unique!\n";
     }
     for(int i=0;i<count;i++){
         cout<<arr2[i]<<",";
